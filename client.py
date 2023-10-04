448930b14a5c9c3074396d556d7abcad861973d7
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-10-04 14:57:02 -0700

    Getting close

--- client.py
+++ client.py
@@ -1,28 +1,60 @@
 from globalVariables import *
 import socket
+import threading
 
-SERVER = "127.0.1.1"
+exit_event = threading.Event()
+
+SERVER = "172.22.160.1"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setblocking(0)
+client.settimeout(0.5)
 client.connect(ADDR)
 print(f"[Client] Connected to {ADDR}")
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_len = len(message)
-    send_length = str(msg_len).encode(FORMAT)
-    send_length += b' ' * (MESSAGEHEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+def send_message():
+    connected = True
+    while connected:
+        if exit_event.is_set():
+            connected = False
+            client.close()
+            break
+
+        print("[Client] Input message to send:")
+        msg = input()
+        
+        message = msg.encode(FORMAT)
+        msg_len = len(message)
+        send_length = str(msg_len).encode(FORMAT)
+        send_length += b' ' * (MESSAGEHEADER - len(send_length))
+        try:
+            client.send(send_length)
+            client.send(message)
+        except:
+            pass
+
+        if msg.lower() == DISCONNECT_MESSAGE:
+            connected = False
+            break
 
 def main():
     connected = True
+    send = threading.Thread(target=send_message)
+    send.start()
+    
     while connected:
-        print("[Client] Input message to send:\n")
-        msg = input()
-        send(msg)
-        if msg == DISCONNECT_MESSAGE:
+        try:
+            message = client.recv(2048).decode(FORMAT)
+            print(message)
+            if message == "[Server] Server shut down. You will be disconnected":
+                exit_event.set()
+                connected = False
+                send.join()
+        except:
+            pass
+
+        if send.is_alive() == False:
             connected = False
-            client.close()
+    client.close()
 main()
--- server.py
+++ server.py
@@ -3,42 +3,140 @@ import threading
 from globalVariables import *
 import os
 
+print(os.getpid())
+exit_event = threading.Event()
+
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
 # creates socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("[Server] Socket Created")
+server.setblocking(0)
+server.settimeout(0.5)
+print("[Server] Socket created")
 
 # bind to the port
 server.bind(ADDR)
 print(f"[Server] Server bound to {ADDR}")
 
-def handle_client(conn, addr):
-    print(os.getpid())
-    print(f"[Server] New connection : {addr}")
+# used to broadcast messages to clients
+broadcast_message = []
+# list of clients that are currently connected to the server
+client_list = []
 
+def handle_client(conn, addr):
+    conn.setblocking(0)
+    conn.settimeout(0.5)
+    print(f"[Server] New connection : {addr}")
     connected = True
+    # used to know what broadcasted messages the client has recieved
+    recieved_messages = 0
+
     while connected:
-        msg_len = conn.recv(MESSAGEHEADER).decode(FORMAT)
-        if msg_len:
-            msg_len = int(msg_len)
-            msg = conn.recv(msg_len).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
+        if exit_event.is_set():
+            try:
+                conn.send("[Server] Server shut down. You will be disconnected".encode(FORMAT))
+            finally:
+                disconnect_client(connected, conn, addr)
+                break
+        try:
+            msg_len = conn.recv(MESSAGEHEADER).decode(FORMAT)
+            if msg_len:
+                msg_len = int(msg_len)
+                msg = conn.recv(msg_len).decode(FORMAT)
+                print(f"[{addr}] {msg}")
+
+                try:
+                    args = msg.split(maxsplit=1)[1]
+                except:
+                    args = None
+                finally:
+                    command = msg.split(maxsplit=1)[0].lower()
+                
+                if command == DISCONNECT_MESSAGE:
+                    print(f"[{addr}] Client disconnected")
+                    conn.send("[Server] Client disconnected".encode(FORMAT))
+                    disconnect_client(connected, conn, addr)
+                    break
+                elif command == BROADCAST_MESSAGE:
+                    broadcast(f"[{addr[1]}]", args)
+                elif command == LIST_CLIENT_MESSAGE:
+                    clients = ""
+                    for i in range(len(client_list)):
+                        clients += client_list[i] + "/n"
+                    clients += client_list[len(client_list)]
+                    conn.send(clients.encode(FORMAT))
+        except:
+            pass
+
+        if len(broadcast_message) > recieved_messages:
+            conn.send(f"{broadcast_message[recieved_messages][0]} {broadcast_message[recieved_messages][1]}".encode(FORMAT))
+            print("[Server] Message Sent")
+            recieved_messages += 1
+        
+    conn.close()
+    print(f"[Server] Active Connections {len(client_list)}")
+
+def disconnect_client(connected, conn, addr):
+    connected = False
+    client_list.remove(addr[1])
     conn.close()
 
-def start():
+def listen():
     # listen for connections
     server.listen()
     print(f"[Server] Server is listening on {SERVER}")
-    while True:
-        # when connection is established create a new thread of handle_client
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[Server] Active Connections {threading.active_count() - 1}")
+    listening = True
+    while listening:
+        if exit_event.is_set():
+            print("[Server] No longer listening...")
+            listening = False
+            break
+        try:
+            # when connection is established create a new thread of handle_client
+            conn, addr = server.accept()
+            client_list.append(addr[1])
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[Server] Active Connections {threading.active_count() - 2}")
+        except:
+            pass
+
+def broadcast(sender, message):
+    broadcast_message.append((sender, message))
+    print(f"{sender} {message}")
+
+def start():
+    # start listen thread
+    listening = threading.Thread(target=listen)
+    listening.start()
+    
+    run = True
+    while run:
+        cin = input()
+        
+        try:
+            args = cin.split(maxsplit=1)[1]
+        except:
+            args = None
+        finally:
+            command = cin.split(maxsplit=1)[0]
+
+        if command.lower() == DISCONNECT_MESSAGE and args == None:
+            exit_event.set()
+            listening.join()
+            print("[Server] Server is stopping...")
+            run = False
+            break
+        elif command.lower() == BROADCAST_MESSAGE:
+            broadcast("[Server]", args)
+        elif command == LIST_CLIENT_MESSAGE:
+            clients = ""
+            for x in client_list:
+                clients += f"{x}\n" 
+            print(clients, end='')
+        else:
+            print("Not a valid command")
 
 print("[Server] Server is starting...")
 start()
