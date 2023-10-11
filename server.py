196a615b19ad6a7fba420fb3d219fc60e526b134
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-10-10 17:44:34 -0700

    FINISHED MULTICASTING

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import socket
 import threading
+import time
+import pandas as pd
 from globalVariables import *
 import os
 
 print(os.getpid())
 exit_event = threading.Event()
 
+df = pd.DataFrame({'Type':[], 'Time(s)':[], 'Source_IP':[], 'Destination_IP':[], 'Source_Port':[], 'Destination_Port':[], 'Length(bytes)':[]})
+starttime = time.time()
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
@@ -35,6 +39,8 @@ def handle_client(conn, addr):
     connected = True
     # used to know what broadcasted messages the client has recieved
     recieved_messages = 0
+    # joined groups
+    joined_groups = []
 
     while connected:
         if exit_event.is_set():
@@ -62,12 +68,26 @@ def handle_client(conn, addr):
                     conn.send("[Server] Client disconnected".encode(FORMAT))
                     disconnect_client(connected, conn, addr)
                     break
+
                 elif command == BROADCAST_MESSAGE:
-                    broadcast(f"[{addr[1]}]", args)
+                    broadcast(addr, args)
+
                 if command == JOIN_GROUP:
-                    join_group(conn, args)
+                    join_group(addr, args)
+                    joined_groups.append([args, 0])
+                    conn.send(f"Successfully joined {args}".encode(FORMAT))
+
                 elif command == MULTICAST_MESSAGE:
-                    multicast(f"[{addr[1]}", args)
+                    try:
+                        message = msg.split(maxsplit=2)[2]
+                        args = msg.split(maxsplit=2)[1]
+                    except:
+                        message = None
+                        args = None
+                    finally:
+                        multicast(addr, args, message)
+                        conn.send(f"Sent {message} to {args}".encode(FORMAT))
+
                 elif command == LIST_CLIENT_MESSAGE:
                     clients = ""
                     for i in range(len(client_list)):
@@ -79,9 +99,19 @@ def handle_client(conn, addr):
 
         if len(broadcast_message) > recieved_messages:
             conn.send(f"{broadcast_message[recieved_messages][0]} {broadcast_message[recieved_messages][1]}".encode(FORMAT))
+            logging("Broadcast", broadcast_message[recieved_messages][0][0], addr[0], broadcast_message[recieved_messages][0][1], addr[1], broadcast_message[recieved_messages][1])
             print("[Server] Message Sent")
             recieved_messages += 1
         
+        for x in joined_groups:
+            for y in client_groups:
+                if x[0] == y[0]:
+                    if x[1] < len(y)-1:
+                        conn.send(f"{y[x[1]+1][0][1]} {y[x[1]+1][1]} {y[x[1]+1][2]}".encode(FORMAT))
+                        logging("Multicast", y[x[1]+1][0][0], addr[0], y[x[1]+1][0][1], addr[1], y[x[1]+1][2])
+                        x[1] += 1
+
+
     conn.close()
     print(f"[Server] Active Connections {len(client_list)}")
 
@@ -114,16 +144,29 @@ def broadcast(sender, message):
     broadcast_message.append((sender, message))
     print(f"{sender} {message}")
 
-def join_group(conn, group_name):
-    if group_name not in client_groups:
-        client_groups[group_name] = []
-    client_groups[group_name].append(conn)
-    conn.send(f"[Server] Joined group '{group_name}'".encode(FORMAT))
+def join_group(addr, group_name):
+    if len(client_groups) == 0:
+        client_groups.append([group_name])
+    for x in client_groups:
+        if group_name not in x:
+            client_groups.append([group_name])
+    print(f"[{addr[0]}] Joined {group_name}")
 
-def multicast(sender, message, group_name):
-    if group_name in client_groups:
-        for client in client_groups[group_name]:
-            client.send(f"{sender} (group '{group_name}'): {message}".encode(FORMAT))
+def multicast(sender, group_name, message):
+    for x in client_groups:
+        if group_name in x:
+            x.append((sender, group_name, message))
+            print(f"{sender} {group_name} {message}")
+
+def logging(name, srcIP, dstIP, srcPort, dstPort, msg):
+    # df = pd.DataFrame({'Type':[], 'Time(s)':[], 'Source_IP':[], 'Destination_IP':[], 'Source_Port':[], 'Destination_Port':[], 'Length(bytes)':[]})
+    if msg == None:
+        msglen = 0
+    else:
+        msglen = len(msg)
+    temp = pd.DataFrame({'Type':[name], 'Time(s)':[str(time.time()-starttime)], 'Source_IP':[srcIP], 'Destination_IP':[dstIP], 'Source_Port':[srcPort], 'Destination_Port':[dstPort], 'Length(bytes)':[str(msglen)]})
+    global df
+    df = pd.concat([df, temp], ignore_index=True)
 
 def start():
     # start listen thread
@@ -148,7 +191,7 @@ def start():
             run = False
             break
         elif command.lower() == BROADCAST_MESSAGE:
-            broadcast("[Server]", args)
+            broadcast(ADDR, args)
         elif command.lower() == JOIN_GROUP:
             print("Invalid command: 'JOIN_GROUP' is not allowed through the console.")
         elif command.lower() == MULTICAST_MESSAGE:
@@ -161,6 +204,8 @@ def start():
             print(clients, end='')
         else:
             print("Not a valid command")
+    df.to_csv('Logging.csv')
 
 print("[Server] Server is starting...")
+
 start()
